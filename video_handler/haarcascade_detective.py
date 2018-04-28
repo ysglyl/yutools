7dab002fa1d4dc87dffa23c495cd2d1aff2b64b7
Shigui Yu <shigui.yu@ericsson.com>
2018-04-28 16:58:26 +0800

    fix bug about show image in QLabel.

--- video_handler/haarcascade_detective.py
+++ video_handler/haarcascade_detective.py
@@ -14,14 +14,9 @@ class HaarcascadeDetective(object):
         return self
 
     def get_face(self, image):
-        img = cv2.imread(image, -1)
-        handle_img = img.copy()
+        handle_img = image.copy()
         gray = cv2.cvtColor(handle_img, cv2.COLOR_BGR2GRAY)
         faces = self.cascade_classifier.detectMultiScale(gray, 1.2, 10)
         for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1, cv2.LINE_AA)
-        if len(img[0][0]) == 4:
-            return 4, cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
-        elif len(img[0][0] == 3):
-            return 3, cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        return 0, None
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1, cv2.LINE_AA)
+        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
